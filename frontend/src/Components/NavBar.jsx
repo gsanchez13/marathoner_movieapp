0f946706ee911f9759abf15e90b5a877ac0790d9
gsanchez13 <gisellesanchez@pursuit.org>
2020-02-08 17:15:35 -0500

    navbar functionality and css borders

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -6,22 +6,27 @@ import Shows from './Shows'
 import About from './About';
 
 class NavBar extends Component {
-    // constructor() {
-    //     super()
-    // }
     render() {
-        return(
-            <div className = "navbar" >
-                <Link to="/" className="Home">Tv Watchlist App</Link>{" "}
-                <Link to="/users">Users</Link>{" "}
-                <Link to="/shows">Shows</Link>{" "}
-                <Link to="/about">About</Link>{" "}
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route path="/users" component={Users} />
-                    <Route path="/shows" component={Shows} />
-                    <Route path="/about" component={About} />
-                </Switch>
+        return (
+            <div>
+                <div className="navbar" >
+                    <div className="logo">
+                        <Link to="/" className="Home">Tv Watchlist App</Link>{" "}
+                    </div>
+                    <div className="links">
+                        <Link to="/users">Users</Link>{" "}
+                        <Link to="/shows">Shows</Link>{" "}
+                        <Link to="/about">About</Link>{" "}
+                    </div>
+                </div>
+                <div>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path="/users" component={Users} />
+                        <Route path="/shows" component={Shows} />
+                        <Route path="/about" component={About} />
+                    </Switch>
+                </div>
             </div>
         )
     }
