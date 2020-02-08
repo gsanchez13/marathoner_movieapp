b4685f48faf81738f90f946a48da194538c2c937
gsanchez13 <gisellesanchez@pursuit.org>
2020-02-08 13:30:44 -0500

    posts new show route

--- backend/routes/shows.js
+++ backend/routes/shows.js
@@ -31,4 +31,19 @@ router.get('/:id', async(req, res, next) => {
     }
 });
 
+router.post('/', async(req, res, next) => {
+    const { title, img_url, user_id, genre_id } = req.body;
+    try {
+        let postedShow = await db.one('INSERT INTO shows(title, img_url, user_id, genre_id) VALUES($1, $2, $3, $4) RETURNING *;', [title, img_url, user_id, genre_id]);
+        res.status(200)
+        .json({
+            payload: postedShow,
+            success: true
+        })
+    }
+    catch(err) {
+        throw err
+    }
+});
+
 module.exports = router;
