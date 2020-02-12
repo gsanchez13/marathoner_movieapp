27e0604efc89b3a46d6a5ceb6c21693d462ddec6
gsanchez13 <gisellesanchez@pursuit.org>
2020-02-11 21:14:38 -0500

    starts a general about page

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-    // constructor() {
-    //     super()
-    // }
     render() {
         return(
-            <div>
-                <h1>About </h1>
+            <div className="about-page">
+                <h1>About this Webpage</h1>
+                <p>
+                    Welcome to the about page. Once again, made with Redux for your entertainment. Please feel free to sign up and contact Giselle Sanchez for any inquiries as she built this page.
+                </p>
+                <br/>
+                <h3 id="cheers">Cheers.</h3>
             </div>
         )
     }
