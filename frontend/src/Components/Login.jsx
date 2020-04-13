bdc35958a2da5d2a4a8b5c0310d28f581d67ae9a
gsanchez13 <gisellesanchez@pursuit.org>
2020-04-12 20:55:22 -0400

    adds hooks to login component for inputs

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Login extends Component {
-    render() {
-        return (
-            <div>
-                Login
-            </div>
-        )
+const Login = () => {
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+    });
+    //state and setState are two items in the useState component of React
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
     }
-}
+    const { username, password } = state;
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input name="username" value={username} onChange={handleChange} placeholder="Username"/>
+                <input name="password" value={password} onChange={handleChange} placeholder="Password"/>
+            </form>
+        </div>
+    )
+};
 export default Login;
