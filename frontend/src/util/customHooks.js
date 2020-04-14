23c59ebd29ef8799685278eccd35c452359b510e
gsanchez13 <gisellesanchez@pursuit.org>
2020-04-14 13:12:07 -0400

    adds hook to view or hide password'

--- frontend/src/util/customHooks.js
+++ frontend/src/util/customHooks.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
 
+
+const useErrorHandler = (false) => {
+    const [ error, showError ] = useState(false);
+    const handleError = (e) => {
+        showError(e.target.value)
+    }
+};
+
 export const useInput = (intitalValue) => {
     const [value, setValue] = useState(intitalValue);
     const handleChange = (e) => {
@@ -7,4 +15,17 @@ export const useInput = (intitalValue) => {
     }
     return { value, onChange: handleChange}
 };
-//useState returns an array of length 2, the first element is a value and the second is a function to set the value
+// useState returns an array of length 2, the first element is a value and the second is a function to set the value
+export const useLogin = (initalValue) => {
+    const [user, setUser] = useState(initalValue);
+    const handleUser = (e) => {
+        setUser(e.target.user)
+    }
+    return { user, onChange: handleUser};
+}
+export const useUsername = (initalValue) => {
+    const [ username, setUsername ] = useState(initalValue);
+    const handleChange = (e) => {
+        setUsername()
+    }
+}
