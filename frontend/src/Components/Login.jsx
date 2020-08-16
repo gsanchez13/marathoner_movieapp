696a1dee6ed996d0d1e3843073504bf9f80b2396
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-16 18:48:06 -0400

    uninstalls material-ui

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-// import clsx from 'clsx';
+import React, { useState } from 'react'; 
 // import axios from 'axios';
 // import { useInput } from '../util/customHooks.js';
 
